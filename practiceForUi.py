b3da3ef1ebceaf3adbd9ce15ba18e643d9f85c91
Zeron01 <64807865+Zeron01@users.noreply.github.com>
2020-08-24 07:06:12 -0400

    Create practiceForUi.py

---
+++ practiceForUi.py
@@ -0,0 +1,27 @@
+from tkinter import*
+
+root = Tk()
+#needed for vs code, otherwise can run the code in IDLE
+#baseWindow = 'This is our Tkinter window'
+theLabel = Label(root,text='This is our tKinter window')
+
+theLabel.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
